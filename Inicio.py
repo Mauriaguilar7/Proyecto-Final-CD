273ce1ea0fc29ed94f8f7234f1fcf0110899ad67
Mauriaguilar7 <00195721@uca.edu.sv>
2024-12-27 23:37:09 -0600

    Add: Inicio terminado 💥

--- Inicio.py
+++ Inicio.py
@@ -11,9 +11,24 @@ st.set_page_config(
 st.title("Proyecto Final")
 
 st.markdown("""
-## Bienvenido
-Este proyecto incluye las siguientes páginas:
 
-- **Inicio**
-- **Análisis Exploratorio**: Gráficas y análisis EDA de los datos.
+Este proyecto busca analizar los datos recopilados por una tienda a través de tarjetas de membresía para entender mejor el comportamiento de sus clientes. A través del análisis de información clave, como el género, la edad, los ingresos y los hábitos de gasto, buscamos identificar patrones que puedan ayudar a la tienda a mejorar sus estrategias comerciales, personalizar su marketing y optimizar la experiencia de compra.
+
+El estudio de estos datos puede generar una comprensión más profunda de los clientes, lo que a su vez puede llevar a tomar decisiones más informadas que impulsen el crecimiento del negocio.
+
+### Integrantes del Proyecto
+
+Este análisis fue realizado por:
+
+- **Gabriel Enrique Cortez Joya**  
+  Correo: [00037021@uca.edu.sv](mailto:00037021@uca.edu.sv)
+  
+- **Denys Eduardo Campos Umanzor**  
+  Correo: [00165221@uca.edu.sv](mailto:00165221@uca.edu.sv)
+  
+- **Gerardo Mauricio Aguilar Chavez**  
+  Correo: [00195721@uca.edu.sv](mailto:00195721@uca.edu.sv)
+  
+- **Wilmer Arnoldo Hernandez Sorto**  
+  Correo: [00146520@uca.edu.sv](mailto:00146520@uca.edu.sv)
 """)
