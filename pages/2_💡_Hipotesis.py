421c0ef242ace9e89a4f264c4ad15db35bcd2c97
Wilmer Hernandez <00146520@uca.edu.sv>
2024-12-27 23:39:56 -0600

    feature: Hipotesis page finished

--- pages/2_💡_Hipotesis.py
+++ pages/2_💡_Hipotesis.py
@@ -3,5 +3,74 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from src.hipotesis import (
-    plot_annualIncome_Age
+    plot_annualIncome_Age,
+    plot_SpendingScore_Age,
+    plot_AnnualIncome_Gender,
+    statistical_testAnnualIncome_Gender,
+    plot_SpendingScore_AgeGroup,
+    plot_workExperience_AgeGroup
 )
+
+# Carga del DataFrame original
+@st.cache_data
+def load_data():
+    return pd.read_csv("data/Customers.csv")
+
+# Configuración de la página
+st.title("Validación de hipótesis planteadas")
+
+# Cargar datos
+df = load_data()
+
+st.header("Hipótesis 1: Las personas al ser más jóvenes presentan menos ingresos anuales.")
+fig = plot_annualIncome_Age(df)  # Solo se retorna una figura
+st.pyplot(fig)  # Mostrar la figura usando Streamlit
+st.write("""
+Se puede observar que la correlación entre el ingreso anual y las edades de los sujetos es aproximadamente nula, lo que significa que no necesariamente si aumenta una variable la otra aumentará tambien. Además podría significar que hay mas que factores que influyen en el ingreso anual de las personas, por lo que no se respalda la hipótesis de que a menor edad menor es el ingreso anual.
+""")
+
+st.header("Hipótesis 2: El puntaje de gastos está directamente relacionado con el tamaño de la familia.")
+st.write(f"""La correlación entre gastos con tamaño de familia es de: 
+         
+         {plot_SpendingScore_Age(df)}
+""")
+st.write("""
+         El análisis de los datos indican que no existe una relación significativa entre el puntaje de gastos y el tamaño de la familia. Esto se respalda en el resultado de la correlación, que muestra un valor cercano a cero. Por lo tanto, se puede decir que el tamaño de la familia no tiene un impacto relevante o significativo en el puntaje de gastos.
+         """)
+
+
+
+st.header("Hipótesis 3: La cantidad de ingresos anuales son indiferentes al genero de las personas.")
+fig = plot_AnnualIncome_Gender(df)  
+st.pyplot(fig)  # Mostrar la figura usando Streamlit
+
+t_stat, p_value = statistical_testAnnualIncome_Gender(df)
+
+st.write(f"""
+Estadístico t: {t_stat}
+
+P-valor: {p_value}
+""")
+
+
+st.write("""
+Analizando el gráfico de ingresos anuales vs género es posible observar que las medias de ingresos por género son aproximadamente iguales. Además al realizar el análisis estadístico con la prueba t de Student se puede respaldar la hipótesis de que los ingresos anuales son inderentes al género gracias a la comparación entre medias de cada género.
+""")
+
+
+st.header("Hipótesis 4: El gasto de las personas menores a 30 años es menor a la de las personas de 30 años o más.")
+fig = plot_SpendingScore_AgeGroup(df)
+st.pyplot(fig)  # Mostrar la figura usando Streamlit
+
+st.write(""" 
+Con base en el análisis representado en la gráfica, se observa que el puntaje de gasto promedio de las personas menores de 30 años es ligeramente mayor que el de las personas de 30 años o más, lo que contradice la hipótesis. Esto indica que los datos analizados no respaldan la afirmación planteada, ya que muestran un resultado diferente al esperado.
+""")
+
+
+st.header("Hipótesis 5: Las personas debajo de los 25 años de edad tienen menor cantidad de experiencia laboral que aquellos sobre la edad de 25.")
+fig = plot_workExperience_AgeGroup(df)
+st.pyplot(fig)  # Mostrar la figura usando Streamlit
+
+st.write(""" 
+La gráfica refleja que las personas menores de 25 años presentan un promedio de experiencia laboral mayor al de las personas de 25 años o más. Este resultado es inusual, ya que contradice la tendencia general donde la experiencia laboral tiende a aumentar con la edad, lo que podría responder a factores específicos utilizados para este estudio.
+         """)
--- src/hipotesis.py
+++ src/hipotesis.py
@@ -2,12 +2,93 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
+from scipy.stats import ttest_ind
+
 
 
 def plot_annualIncome_Age(df):
-    plt.figure(figsize=(8, 6))
-    sns.regplot(x='Age', y='Annual Income ($)', data=df, scatter_kws={'alpha': 0.6}, line_kws={'color': 'red'})
-    plt.title('Relación entre Edad e Ingresos Anuales (con regresión)')
-    plt.xlabel('Edad')
-    plt.ylabel('Ingresos Anuales')
-    plt.show() 
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sns.regplot(x='Age', y='Annual Income ($)', data=df, scatter_kws={'alpha': 0.6}, line_kws={'color': 'red'}, ax=ax)
+    ax.set_title('Relación entre Edad e Ingresos Anuales (con regresión)')
+    ax.set_xlabel('Edad')
+    ax.set_ylabel('Ingresos Anuales')
+    return fig
+
+def plot_SpendingScore_Age(df):
+    return df['Spending Score (1-100)'].corr(df['Family Size'])
+
+
+def plot_AnnualIncome_Gender(df):
+    # Agrupamos los datos y calculamos el promedio
+    AnnualIncome_Gender = df.groupby('Gender')['Annual Income ($)'].mean()
+
+    # Crear figura y eje
+    fig, ax = plt.subplots(figsize=(12, 6))
+    sns.barplot(x=AnnualIncome_Gender.index, y=AnnualIncome_Gender.values, palette="viridis", ax=ax)
+
+    # Configuración del gráfico
+    ax.set_title("Ingresos anuales y género")
+    ax.set_xlabel("Género")
+    ax.set_ylabel("Ingreso anual ($)")
+    ax.set_xticks(range(len(AnnualIncome_Gender.index)))
+    ax.set_xticklabels(AnnualIncome_Gender.index, rotation=45)
+
+    return fig
+
+
+def statistical_testAnnualIncome_Gender(df):
+    # Filtrar ingresos por género
+    male_income = df[df['Gender'] == 'Male']['Annual Income ($)']
+    female_income = df[df['Gender'] == 'Female']['Annual Income ($)']
+
+    # Prueba t de Student
+    t_stat, p_value = ttest_ind(male_income, female_income)
+
+    return t_stat, p_value
+
+
+def plot_SpendingScore_AgeGroup(df):
+    # Crear una nueva columna que agrupe por menores y mayores de 30 años
+    df['Age Group'] = pd.cut(df['Age'], bins=[0, 30, df['Age'].max()], labels=['Menores de 30', '30 o más'])
+
+    # Agrupamos por los intervalos de edad y calculamos el promedio de gasto
+    SpendingScore_AgeGroup = df.groupby('Age Group')['Spending Score (1-100)'].mean()
+
+    # Convertimos el índice en cadenas para visualización en el eje X
+    SpendingScore_AgeGroup.index = SpendingScore_AgeGroup.index.astype(str)
+
+    # Usar la paleta de colores
+    fig, ax = plt.subplots(figsize=(12, 6))
+    sns.barplot(x=SpendingScore_AgeGroup.index, y=SpendingScore_AgeGroup.values, palette="viridis")
+
+    # Configuración del gráfico
+    plt.title("Promedio de gasto por grupo de edad", fontsize=14)
+    plt.xlabel("Grupo de Edad", fontsize=12)
+    plt.ylabel("Puntaje de Gasto (1-100)", fontsize=12)
+    plt.ylim(0, 100)
+    plt.xticks(rotation=0)
+
+    return fig
+
+def plot_workExperience_AgeGroup(df):
+    # Crear intervalos de edades de 5 en 5 usando pd.cut()
+    df['Age Group'] = pd.cut(df['Age'], bins=[0, 25, df['Age'].max()], labels=['Menores de 25', '25 o más'])
+
+    # Agrupamos por los intervalos de edad y calculamos el promedio
+    WorkExperience_AgeGroup = df.groupby('Age Group')['Work Experience'].mean()
+
+    # Convertimos los intervalos en cadenas para visualización en el eje X
+    WorkExperience_AgeGroup.index = WorkExperience_AgeGroup.index.astype(str)
+
+    # Usar la paleta de colores de Seaborn automáticamente
+    fig, ax = plt.subplots(figsize=(12, 6))
+    sns.barplot(x=WorkExperience_AgeGroup.index, y=WorkExperience_AgeGroup.values, palette="viridis")
+
+    # Configuración del gráfico
+    plt.title("Experiencia laboral promedio por grupo de edad")
+    plt.xlabel("Grupo de Edad")
+    plt.ylabel("Experiencia laboral promedio (años)")
+    plt.ylim(0, 10)
+    plt.xticks(rotation=0)
+
+    return fig
